e78f3318d4ccfde29873a1e6449aedf872ca6ccc
fabiocentrella1994@gmail.com <fabiocentrella1994@gmail.com>
2020-07-19 16:38:47 +0100

    all issues closed

--- backend/app.js
+++ backend/app.js
@@ -8,38 +8,46 @@
 // MongoDb Name: TablesAdmin
 // Password: 12345
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-mongoose.set('useCreateIndex', true);
-const path = require('path');
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+mongoose.set('useCreateIndex', true)
+const path = require('path')
 
-const sauceRoutes = require('./routes/sauce');
-const userRoutes = require('./routes/user');
+const sauceRoutes = require('./routes/sauce')
+const userRoutes = require('./routes/user')
 
-const app = express();
+const app = express()
 
-mongoose.connect('mongodb+srv://Fabio:13%2FAgosto%2F94@cluster0-iabbj.mongodb.net/SoPekocko?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose
+  .connect(
+    'mongodb+srv://Fabio:13%2FAgosto%2F94@cluster0-iabbj.mongodb.net/SoPekocko?retryWrites=true&w=majority',
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
   .then(() => {
-    console.log('Successfully connected to MongoDB Atlas!');
+    console.log('Successfully connected to MongoDB Atlas!')
   })
-  .catch((error) => {
-    console.log('Unable to connect to MongoDB Atlas!');
-    console.error(error);
-  });
+  .catch(error => {
+    console.log('Unable to connect to MongoDB Atlas!')
+    console.error(error)
+  })
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-});
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8081')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  )
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+  )
+  next()
+})
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/api/sauces', sauceRoutes)
+app.use('/api/auth', userRoutes)
 
-app.use('/api/sauces', sauceRoutes);
-app.use('/api/auth', userRoutes);
-
-module.exports = app;
+module.exports = app
